5ba70f6aadce9acb3806f85b65dc0f2694956053
shahramhal <156790792+shahramhal@users.noreply.github.com>
2025-03-13 17:46:36 +0000

    #

--- Users/models.py
+++ Users/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
+class User(models.Model):
+    userID = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)  # In a real app, you'd use Django's auth system instead
+    userType = models.CharField(max_length=50) 
+    managerID = models.IntegerField(null=True, blank=True)  # Allow NULL values
+    
+    def __str__(self):
+        return self.username
