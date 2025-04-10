b67bb46d9d11b57cd560faec38250a36284f2493
mkhizi <180230034+khiziii@users.noreply.github.com>
2025-04-11 00:22:48 +0100

    implementation of charts
    changes

--- Teams/views.py
+++ Teams/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def teams(request):
-    # Render the teams.html template when the /teams/ URL is accessed
-    return render(request, 'DeptLeadpg2.html')
+
+
+
+def SenManagerp1(request):
+    return render(request, 'SenManagerp1.html' ) 
