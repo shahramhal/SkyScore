b0f10876083b71130ed7de0b75266770d5ad890a
mkhizi <180230034+khiziii@users.noreply.github.com>
2025-04-18 00:14:22 +0100

    Changes
    Made some changes (incomplete ones) + created three pages for teamleader

--- Teams/models.py
+++ Teams/models.py
@@ -9,3 +9,13 @@ class HealthCheckCardUsage(models.Model):
 
     def __str__(self):
         return f"{self.cardName} - {self.value}"
+
+
+class DepartmentPerformance(models.Model):
+    department = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)  # e.g., Mission, Fun, etc.
+    score = models.FloatField()
+    contribution = models.FloatField()  # for pie chart (optional)
+
+    def __str__(self):
+        return f"{self.department} - {self.category}"
--- Teams/urls.py
+++ Teams/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     # Include URLs from the Users app
-    path("",views.SenManagerp1, name="SenMan")
-
-    # Add your Teams app URLs here if needed
+    path('senmanager/', views.department_dashboard, name='sen_manager'),
+     # Add your Teams app URLs here if needed
     # path('', include('Teams.urls')),
+
+    path('get_departments/', views.get_departments, name='get_departments'),
+    path('get_department_data/', views.get_department_data, name='get_department_data')
 ]
