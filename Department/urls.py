3a7e65a1bd519c6f6b5ffcd1d09f6e896ee9ff5f
Imaad117 <imaad.malik@outlook.com>
2025-04-29 23:50:44 +0100

    add
    deptleader page

---
+++ Department/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('departmentDashboard/', views.departmentDashboard, name='departmentDashboard'),
+    path('department/settings/', views.department_settings, name='department_settings'),
+    path('get_data/', views.get_data, name='get_data'),
+    path('view_summary/<int:team_id>/', views.view_summary, name='view_summary'),
+]
