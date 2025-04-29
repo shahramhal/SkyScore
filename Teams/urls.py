690a2498b57f2b58a7c3182beb3f664859651a2e
mkhizi <180230034+khiziii@users.noreply.github.com>
2025-04-29 22:19:56 +0100

    change in summary page senman
    change

--- Teams/urls.py
+++ Teams/urls.py
@@ -4,10 +4,13 @@ from . import views
 urlpatterns = [
     # Include URLs from the Users app
     path('senmanager/', views.getsenman_overview, name='SenManagerDash'),
+    path('getsenmanagerprog/', views.getsenmanprogress, name='SenManagerprog'),
+    
     path('department-summary/',views.department_summary, name='department_summary'),
     path('api/department-data/', views.department_data, name='department_data'),
     path('teamleaddash/', views.team_lead_dashboard, name='teamleaddash'),
     path('teamleadprog/', views.team_progress, name='teamleadprog'),
+    path('api/engineering-metrics/', views.engineering_metrics, name='engineering_metrics'),
     
     
     
