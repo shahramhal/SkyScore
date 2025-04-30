cd4b0865424885af501032bd401c85173e3d3700
Imaad117 <imaad.malik@outlook.com>
2025-04-30 02:39:37 +0100

    add
    add

--- Department/urls.py
+++ Department/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('departmentDashboard/', views.departmentDashboard, name='departmentDashboard'),
+    path('departmentDashboard/', views.departmentDashboard, name='dept_lead_dashboard'),
     path('department/settings/', views.department_settings, name='department_settings'),
     path('get_data/', views.get_data, name='get_data'),
     path('view_summary/<int:team_id>/', views.view_summary, name='view_summary'),
